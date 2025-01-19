efccd3e026bab9e32874a6841b1c446029eb0c0e
EdvardKenua <orekhovsemyon@gmail.com>
2025-01-19 19:23:29 +0300

    created base object model in models.py

--- objects/models.py
+++ objects/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+
+class TypeObject(models.Model):
+    name = models.CharField(unique=True, max_length=255)
+
+
+class Category(models.Model):
+    name = models.CharField(unique=True, max_length=255)
+
+
+class Region(models.Model):
+    name = models.CharField(unique=True, max_length=255)
+
+
+class Banner(models.Model):
+    name = models.CharField(unique=True, max_length=255)
+
+
+class Object(models.Model):
+    url_path = models.URLField()
+    is_showing = models.BooleanField(default=True, db_index=True)
+    short_name = models.CharField(max_length=255)
+    cost = models.IntegerField(db_index=True)
+    type = models.ForeignKey(to=TypeObject, on_delete=models.SET_NULL, null=True, db_index=True)
+    category = models.ForeignKey(to=Category, on_delete=models.SET_NULL, null=True, db_index=True)
+    city = models.CharField(max_length=255, null=True, db_index=True)
+    banner = models.ForeignKey(to=Banner, on_delete=models.SET_NULL, null=True, db_index=True)
+    space = models.FloatField(null=True, db_index=True)
+    description = models.TextField(null=True)
+    conditions_accommodation = models.TextField(null=True)
+    contacts = models.TextField(null=True)
+    finding_description = models.TextField(null=True)
+    helpful_info = models.TextField(null=True)
+    parking_info = models.TextField(null=True)
+
+
+
+
