bd34670fb9d07f1c2783472c039b867d935abdc1
EdvardKenua <orekhovsemyon@gmail.com>
2025-03-30 19:27:41 +0700

    fixed: serializers

--- contents/serializers.py
+++ contents/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
-from .models import Article
+from .models import Article, MediaFile
 
+class MediaFileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MediaFile
+        fields = '__all__'
 
 class ArticleSerializer(serializers.ModelSerializer):
+    media = MediaFileSerializer(many=True, read_only=True)
+
     class Meta:
         model = Article
-        fields = ['id', 'title', 'content', 'publication_date', 'photo_video', 'short_description']
+        fields = ['id', 'title', 'content', 'publication_date', 'short_description', 'media']
