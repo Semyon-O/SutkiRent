1ed5fe9eadd05ccb3426e81e8e431068170e21db
semyon <orekhovsemyon@gmail.com>
2025-04-09 11:18:06 +0300

    updated filters

--- objects/filters.py
+++ objects/filters.py
@@ -1,16 +1,19 @@
 import django_filters
-from .models import Object, Region, Category, TypeObject
+from .models import Object, Region, Category, TypeObject, Metro
 
 
 class ObjectFilter(django_filters.FilterSet):
     cost = django_filters.RangeFilter(field_name='cost')
     type = django_filters.ModelChoiceFilter(queryset=TypeObject.objects.all())
-    amount_rooms = django_filters.NumberFilter()
-    floor = django_filters.NumberFilter()
+    amount_rooms = django_filters.RangeFilter(field_name="amount_rooms")
+    floor = django_filters.RangeFilter(field_name="floor")
     category = django_filters.ModelChoiceFilter(queryset=Category.objects.all())
     region = django_filters.ModelChoiceFilter(queryset=Region.objects.all())
     city = django_filters.CharFilter(lookup_expr='icontains')
     space = django_filters.RangeFilter(field_name='space')
+    near_metro = django_filters.ModelMultipleChoiceFilter(field_name="near_metro",
+                                                          queryset=Metro.objects.all(),
+                                                          )
 
     booking_date = django_filters.DateFromToRangeFilter(field_name='booking_date', lookup_expr='icontains')
 
